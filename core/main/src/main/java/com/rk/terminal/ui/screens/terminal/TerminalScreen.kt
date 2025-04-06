a80edb7ee19c2c8db5f4e72b71a93552a1851b81
RohitKushvaha01 <oldisg131@gmail.com>
2025-04-06 17:19:45 +0530

    fix. navigation bar paddings

--- core/main/src/main/java/com/rk/terminal/ui/screens/terminal/TerminalScreen.kt
+++ core/main/src/main/java/com/rk/terminal/ui/screens/terminal/TerminalScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -367,7 +368,7 @@ fun TerminalScreen(
                             }
 
                             val density = LocalDensity.current
-                            Column(modifier = Modifier.imePadding().padding(top = if (showToolbar.value){0.dp}else{
+                            Column(modifier = Modifier.imePadding().navigationBarsPadding().padding(top = if (showToolbar.value){0.dp}else{
                                 with(density){
                                     TopAppBarDefaults.windowInsets.getTop(density).toDp()
                                 }
