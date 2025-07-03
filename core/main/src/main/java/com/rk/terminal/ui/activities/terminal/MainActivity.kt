6c9a1b8690a22733493722baac8737e2ad3f2cb9
RohitKushvaha01 <oldisg131@gmail.com>
2025-07-03 17:37:15 +0530

    feat. show restore keyboard state #24

--- core/main/src/main/java/com/rk/terminal/ui/activities/terminal/MainActivity.kt
+++ core/main/src/main/java/com/rk/terminal/ui/activities/terminal/MainActivity.kt
@@ -6,10 +6,13 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -24,6 +27,7 @@ import androidx.navigation.compose.rememberNavController
 import com.rk.terminal.service.SessionService
 import com.rk.terminal.ui.navHosts.MainActivityNavHost
 import com.rk.terminal.ui.screens.terminal.TerminalScreen
+import com.rk.terminal.ui.screens.terminal.terminalView
 import com.rk.terminal.ui.theme.KarbonTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -80,12 +84,43 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    var isKeyboardVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         if (intent.hasExtra("awake_intent")){
             moveTaskToBack(true)
+        }
+
+    }
+
+    var wasKeyboardOpen = false
+    override fun onPause() {
+        super.onPause()
+        wasKeyboardOpen = isKeyboardVisible
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val rootView = findViewById<View>(android.R.id.content)
+        rootView.viewTreeObserver.addOnGlobalLayoutListener {
+            val rect = Rect()
+            rootView.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = rootView.rootView.height
+            val keypadHeight = screenHeight - rect.bottom
+            val isVisible = keypadHeight > screenHeight * 0.15
+
+            isKeyboardVisible = isVisible
+        }
+
+
+        if (wasKeyboardOpen && !isKeyboardVisible){
+            terminalView.get()?.let {
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(it, InputMethodManager.SHOW_IMPLICIT)
+            }
         }
     }
 }
